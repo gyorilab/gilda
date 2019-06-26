020c120dc429159b3403209a9e7e27eff138a296
Ben Gyori <ben.gyori@gmail.com>
2019-06-25 22:22:08 -0400

    Add an implementation of the Term class

---
+++ grounding/term.py
@@ -0,0 +1,50 @@
+
+class Term(object):
+    """Represents a text entry corresponding to a grounded term.
+
+    Attributes
+    ----------
+    norm_text : str
+        The normalized text corresponding to the text entry, used for lookups.
+    text : str
+        The text entry itself.
+    db : str
+        The database / name space corresponding to the grounded term.
+    id : str
+        The identifier of the grounded term within the database / name space.
+    entry_name : str
+        The standardized name corresponding to the grounded term.
+    status : str
+        The relationship of the text entry to the grounded term, e.g., synonym.
+    """
+    def __init__(self, norm_text, text, db, id, entry_name, status):
+        self.norm_text = norm_text
+        self.text = text
+        self.db = db
+        self.id = str(id)
+        self.entry_name = entry_name
+        self.status = status
+
+    def __str__(self):
+        return 'Term(%s,%s,%s,%s,%s,%s)' % (
+            self.norm_text, self.text, self.db, self.id, self.entry_name,
+            self.status)
+
+    def __repr__(self):
+        return str(self)
+
+    def to_json(self):
+        """Return the term serialized into a JSON dict."""
+        return {
+            'norm_text': self.norm_text,
+            'text': self.text,
+            'db': self.db,
+            'id': self.id,
+            'entry_name': self.entry_name,
+            'status': self.status
+        }
+
+    def to_list(self):
+        """Return the term serialized into a list of strings."""
+        return [self.norm_text, self.text, self.db, self.id,
+                self.entry_name, self.status]
