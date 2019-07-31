ee8f865711d8760a1cf9586ed80b889b19048378
Ben Gyori <ben.gyori@gmail.com>
2019-07-31 14:02:56 -0400

    Bump version to 0.1.1

--- doc/conf.py
+++ doc/conf.py
@@ -26,7 +26,7 @@ author = 'Benjamin M. Gyori'
 # The short X.Y version
 version = '0.1'
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
--- gilda/__init__.py
+++ gilda/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 import logging
 
 logging.basicConfig(format=('%(levelname)s: [%(asctime)s] %(name)s'
