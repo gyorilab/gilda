2e2c4b8ab80a83d8e66014ab67f004434b424bee
Ben Gyori <ben.gyori@gmail.com>
2020-01-23 12:56:57 -0500

    Add simple API test

---
+++ gilda/tests/test_api.py
@@ -0,0 +1,15 @@
+from gilda.tests import appreq
+from gilda.api import *
+
+
+def test_api_ground():
+    scores = ground('kras')
+    assert appreq(scores[0].score, 0.9845), scores
+    scores = ground('ROS', 'reactive oxygen')
+    assert scores[0].term.db == 'MESH'
+
+
+def test_get_models():
+    models = get_models()
+    assert len(models) > 500
+    assert 'STK1' in models
