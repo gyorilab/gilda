fcd514a305eb79beb87a2db181495aafd51cf063
Ben Gyori <ben.gyori@gmail.com>
2020-02-14 16:08:53 -0500

    Implement function to get all names for an entity

--- gilda/api.py
+++ gilda/api.py
@@ -27,6 +27,16 @@ class GrounderInstance(object):
     def get_models(self):
         return sorted(list(self.get_grounder().gilda_disambiguators.keys()))
 
+    def get_names(self, db, id, status=None, source=None)
+        names = []
+        for entries in self.get_grounder().entries.values():
+            for entry in entries:
+                if (entry.db == db) and (entry.id == id) and \
+                        (not status or entry.status == status) and \
+                        (not source or entry.source == source):
+                    names.append(entry.text)
+        return sorted(list(set(names)))
+
 
 grounder = GrounderInstance()
 
@@ -61,3 +71,22 @@ def get_models():
         available.
     """
     return grounder.get_models()
+
+
+def get_names(db, id, status=None, source=None):
+    """Return a list of entity texts corresponding to a given database ID.
+
+    Parameters
+    ----------
+    db : str
+        The database in which the ID is an entry, e.g., HGNC.
+    id : str
+        The ID of an entry in the database.
+    status : Optional[str]
+        If given, only entity texts with the given status e.g., "synonym"
+        are returned.
+    source : Optional[str]
+        If given, only entity texts from the given source e.g., "uniprot"
+        are returned.
+    """
+    return grounder.get_names(db, id, status=status, source=source)
