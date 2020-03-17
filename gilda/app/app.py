3a76e43f421516bf65c028b15db7b27c069fb6b8
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-03-17 23:22:56 +0100

    Add UI for results

--- gilda/app/app.py
+++ gilda/app/app.py
@@ -1,44 +1,41 @@
-import os
-
 from flask import Flask, Response, abort, jsonify, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
 
 from gilda.api import *
 
 app = Flask(__name__)
-app.secret_key = os.urandom(8)
+app.config['WTF_CSRF_ENABLED'] = False
 Bootstrap(app)
 
 
 class GroundForm(FlaskForm):
-    text = StringField('Text')
+    text = StringField('Text', validators=[DataRequired()])
     context = TextAreaField('Context')
     submit = SubmitField('Submit')
 
+    def get_matches(self):
+        return ground(self.text.data, self.context.data)
 
-@app.route('/', methods=['GET'])
+
+@app.route('/', methods=['GET', 'POST'])
 def info():
-    return render_template('home.html', form=GroundForm())
+    form = GroundForm()
+    if form.validate_on_submit():
+        matches = form.get_matches()
+        return render_template('matches.html', matches=matches, form=form)
+    return render_template('home.html', form=form)
 
 
 @app.route('/ground', methods=['POST'])
 def ground_endpoint():
-    content_type = request.headers["Content-Type"]
-    if "application/json" in content_type:
-        # Get input parameters
-        text = request.json.get('text')
-        context = request.json.get('context')
-    elif 'application/x-www-form-urlencoded' in content_type:
-        # Get input parameters from form
-        form = GroundForm()
-        text = form.text.data
-        context = form.context.data
-    else:
-        return abort(Response('Missing application/json header.'
-                              ' Got {}'.format(content_type), 415))
-
+    if request.json is None:
+        abort(Response('Missing application/json header.', 415))
+    # Get input parameters
+    text = request.json.get('text')
+    context = request.json.get('context')
     scored_matches = ground(text, context)
     res = [sm.to_json() for sm in scored_matches]
     return jsonify(res)
