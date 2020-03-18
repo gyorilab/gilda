b97c4a799bb705417170852066045960ab6dc4c4
Ben Gyori <ben.gyori@gmail.com>
2020-03-17 22:46:35 -0400

    Extend MeSH mappings to CHEBI terms

--- scripts/generate_mesh_mappings.py
+++ scripts/generate_mesh_mappings.py
@@ -1,7 +1,9 @@
 import os
+from collections import defaultdict
 from gilda.generate_terms import generate_famplex_terms, generate_hgnc_terms, \
-    generate_mesh_terms, generate_uniprot_terms, filter_out_duplicates
-from indra.databases import mesh_client, hgnc_client
+    generate_mesh_terms, generate_uniprot_terms, generate_chebi_terms, \
+    filter_out_duplicates
+from indra.databases import mesh_client
 
 
 mesh_protein = 'D000602'
@@ -14,56 +16,61 @@ def dump_mappings(mappings, fname):
     mappings = sorted(mappings.values(), key=lambda x: x[0].id)
     with open(fname, 'w') as fh:
         for me, te in mappings:
-            mesh_name = mesh_client.get_mesh_name(me.id)
-            if te.db == 'HGNC':
-                tname = hgnc_client.get_hgnc_name(te.id)
-            elif te.db == 'FPLX':
-                tname = te.id
-            fh.write('\t'.join([me.db, me.id, mesh_name,
-                                te.db, te.id, tname]) + '\n')
+            fh.write('\t'.join([me.db, me.id, me.entry_name,
+                                te.db, te.id, te.entry_name]) + '\n')
+
+
+def get_ambigs_by_db(ambigs):
+    ambigs_by_db = defaultdict(list)
+    for term in ambigs:
+        ambigs_by_db[term.db].append(term)
+    return dict(ambigs_by_db)
 
 
 def get_mesh_mappings(ambigs):
     predicted_mappings = {}
-    for text, ambig in ambigs.items():
-        hgnc_entries = [a for a in ambig if a.db == 'HGNC']
-        mesh_entries = [a for a in ambig if a.db == 'MESH']
-        fplx_entries = [a for a in ambig if a.db == 'FPLX']
-        if len(mesh_entries) != 1:
+    for text, ambig_terms in ambigs.items():
+        ambigs_by_db = get_ambigs_by_db(ambig_terms)
+        if len(ambigs_by_db.get('MESH', [])) != 1:
             continue
-        me = mesh_entries[0]
+        me = ambigs_by_db['MESH'][0]
         if (mesh_client.mesh_isa(me.id, mesh_protein) or
                 mesh_client.mesh_isa(me.id, mesh_enzyme)):
             print('Considering %s' % me.id)
-            if len(fplx_entries) == 1:
-                key = (me.id, 'FPLX', fplx_entries[0].id)
-                predicted_mappings[key] = (me, fplx_entries[0])
-            elif len(hgnc_entries) == 1:
-                key = (me.id, 'HGNC', hgnc_entries[0].id)
-                predicted_mappings[key] = (me, hgnc_entries[0])
+            if len(ambigs_by_db.get('FPLX', [])) == 1:
+                key = (me.id, 'FPLX', ambigs_by_db['FPLX'][0].id)
+                predicted_mappings[key] = (me, ambigs_by_db['FPLX'][0])
+            elif len(ambigs_by_db.get('HGNC', [])) == 1:
+                key = (me.id, 'HGNC', ambigs_by_db['HGNC'][0].id)
+                predicted_mappings[key] = (me, ambigs_by_db['HGNC'][0])
+        else:
+            if len(ambigs_by_db.get('CHEBI', [])) == 1:
+                key = (me.id, 'CHEBI', ambigs_by_db['CHEBI'][0].id)
+                predicted_mappings[key] = (me, ambigs_by_db['CHEBI'][0])
     return predicted_mappings
 
 
 def find_ambiguities(terms):
-    ambig_entries = {}
+    ambig_entries = defaultdict(list)
     for term in terms:
         # We consider it an ambiguity if the same text entry appears
         # multiple times
-        key = term.text
-        if key in ambig_entries:
-            ambig_entries[key].append(term)
-        else:
-            ambig_entries[key] = [term]
+        ambig_entries[term.text].append(term)
     # It's only an ambiguity if there are two entries at least
     ambig_entries = {k: v for k, v in ambig_entries.items() if len(v) >= 2}
     return ambig_entries
 
 
-if __name__ == '__main__':
+def get_terms():
     terms = generate_mesh_terms(ignore_mappings=True) + \
         generate_hgnc_terms() + generate_famplex_terms() + \
-        generate_uniprot_terms(download=False)
+        generate_uniprot_terms(download=False) + generate_chebi_terms()
     terms = filter_out_duplicates(terms)
+    return terms
+
+
+if __name__ == '__main__':
+    terms = get_terms()
     ambigs = find_ambiguities(terms)
     mappings = get_mesh_mappings(ambigs)
     dump_mappings(mappings, os.path.join(resources, 'mesh_mappings.tsv'))
