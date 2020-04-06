07c5af9411704acd0b107dc9f1a4aead9aa3a6ea
Ben Gyori <ben.gyori@gmail.com>
2020-04-06 00:28:18 -0400

    Bump version to 0.3.0

--- doc/conf.py
+++ doc/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Benjamin M. Gyori'
 author = 'Benjamin M. Gyori'
 
 # The short X.Y version
-version = '0.2'
+version = '0.3'
 # The full version, including alpha/beta/rc tags
-release = '0.2.3'
+release = '0.3.0'
 
 
 # -- General configuration ---------------------------------------------------
