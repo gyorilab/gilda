3b5d10c1e689abef7423274c50008971a3fc6287
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-01-04 17:41:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,10 +33,10 @@ setup(name='gilda',
           'Programming Language :: Python :: 3.7'
       ],
       packages=find_packages(),
-      install_requires=['regex', 'adeft>=0.4.0', 'boto3', 'obonet'],
+      install_requires=['regex', 'adeft>=0.4.0', 'boto3', 'flask',
+                        'flask-wtf', 'bootstrap-flask', 'obonet'],
       extras_require={'test': ['nose', 'coverage'],
                       'terms': ['indra'],
-                      'benchmarks': ['pandas', 'requests'],
-                      'web': ['flask', 'flask-wtf', 'bootstrap-flask']},
-      keywords=['nlp', 'biology'],
+                      'benchmarks': ['pandas', 'requests']},
+      keywords=['nlp', 'biology']
       )
