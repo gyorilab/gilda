be62a482fd73e29993f80caf968246053706e74b
Ben Gyori <ben.gyori@gmail.com>
2021-01-19 23:14:19 -0500

    Move main to make entrypoint work

--- gilda/app/__init__.py
+++ gilda/app/__init__.py
@@ -1,1 +1,12 @@
+import argparse
 from .app import app
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Run the grounding app.')
+    parser.add_argument('--host', default='0.0.0.0')
+    parser.add_argument('--port', default=8001, type=int)
+    args = parser.parse_args()
+    app.run(host=args.host, port=args.port, threaded=False)
+
