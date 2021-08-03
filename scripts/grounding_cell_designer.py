edc6c7305e96af07dae5104fcb5693eb311cdd53
Ben Gyori <ben.gyori@gmail.com>
2021-08-02 22:03:40 -0400

    Refine grounding approach

--- scripts/grounding_cell_designer.py
+++ scripts/grounding_cell_designer.py
@@ -22,6 +22,16 @@ namespaces = {'sbml': 'http://www.sbml.org/sbml/level2/version4',
               'dc': 'http://purl.org/dc/elements/1.1/'}
 
 
+irrelevant_grounding_ns = {'pubmed', 'taxonomy', 'doi', 'wikipathways', 'pdb',
+                           'intact', 'biogrid', 'pmc'}
+relevant_grounding_ns = {'ncbiprotein', 'ncbigene', 'uniprot', 'obo.go',
+                         'obo.chebi', 'pubchem.compound', 'pubchem.substance',
+                         'hgnc', 'hgnc.symbol', 'mesh', 'interpro',
+                         'refseq', 'ensembl', 'ec-code', 'brenda',
+                         'kegg.compound', 'drugbank'}
+irrelevant_classes = {'DEGRADED'}
+
+
 def register_all_namespaces(fname):
     namespaces = dict([node for _, node in
                        ET.iterparse(fname, events=['start-ns'])])
@@ -33,16 +43,22 @@ def add_groundings(et):
     species = et.findall('sbml:model/sbml:listOfSpecies/sbml:species',
                          namespaces=namespaces)
     for species in species:
-        name = species.attrib.get('name')
-        print(name)
+        existing_grounding = get_existing_grounding(species)
+        # Important: this is where we decide if we will add any grounding.
+        # Here we skip this species if it has any relevant grounding
+        if set(existing_grounding) & relevant_grounding_ns:
+            continue
         class_tag = species.find(
             'sbml:annotation/celldesigner:extension/'
             'celldesigner:speciesIdentity/celldesigner:class',
             namespaces=namespaces)
         if class_tag is not None:
-            cls = class_tag.text
-            print(cls)
-        print(get_existing_grounding(species))
+            entity_class = class_tag.text
+            if entity_class in irrelevant_classes:
+                continue
+        name = species.attrib.get('name')
+        print(name)
+        print(entity_class)
         matches = gilda.ground(name)
         if matches:
             print(name, matches[0].term.db, matches[0].term.id,
@@ -54,21 +70,26 @@ def add_groundings(et):
 
 
 def get_existing_grounding(species):
-    grounding_elements = \
-        species.findall('sbml:annotation/rdf:RDF/rdf:Description/'
-                        'bqbiol:isDescribedBy/rdf:Bag/rdf:li',
-                        namespaces=namespaces)
+    # Others: isHomologTo
+    bqbio_tags = ['isDescribedBy', 'isEncodedBy', 'is', 'encodes',
+                  'occursIn']
+
     groundings = defaultdict(list)
-    for element in grounding_elements:
-        urn = element.attrib['{http://www.w3.org/1999/02/22-rdf-syntax-ns#}'
-                             'resource']
-        match = re.match(r'urn:miriam:([^:]+):(.+)', urn)
-        if not match:
-            print('Unmatched urn: %s' % urn)
-            continue
-        else:
-            db_ns, db_id = match.groups()
-            groundings[db_ns].append(db_id)
+    for tag in bqbio_tags:
+        grounding_elements = \
+            species.findall('sbml:annotation/rdf:RDF/rdf:Description/'
+                            'bqbiol:%s/rdf:Bag/rdf:li' % tag,
+                            namespaces=namespaces)
+        for element in grounding_elements:
+            urn = element.attrib['{http://www.w3.org/1999/02/22-rdf-syntax-ns#}'
+                                 'resource']
+            match = re.match(r'urn:miriam:([^:]+):(.+)', urn)
+            if not match:
+                print('Unmatched urn: %s' % urn)
+                continue
+            else:
+                db_ns, db_id = match.groups()
+                groundings[db_ns].append(db_id)
     return groundings
 
 
@@ -114,10 +135,11 @@ def dump(et, fname):
 if __name__ == '__main__':
     base_path = sys.argv[1]
     stable_xmls = list(Path(base_path).rglob('*_stable.xml'))
-    for stable_xml in stable_xmls[:1]:
+    for stable_xml in stable_xmls:
         print('Grounding %s' % stable_xml)
         register_all_namespaces(stable_xml)
         et = ET.parse(stable_xml)
         et = add_groundings(et)
         out_fname = stable_xml.as_posix()[:-4] + '_grounded.xml'
         dump(et, out_fname)
+        print()
