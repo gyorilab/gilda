3cbe14a01cf439ef5553a4c8db03ee51754ece73
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-08-03 18:50:44 +0200

    Update medmentions.py

--- benchmarks/medmentions.py
+++ benchmarks/medmentions.py
@@ -6,16 +6,42 @@ from more_click import verbose_option
 
 from pubtator_loader import from_gz
 
-URL = 'https://github.com/chanzuckerberg/MedMentions/raw/master/full/data/corpus_pubtator.txt.gz'
-MODULE = pystow.module('gilda', 'medmentions')
-CORPUS_PATH = MODULE.join(name='corpus.json')
+URL = "https://github.com/chanzuckerberg/MedMentions/raw/master/full/data/corpus_pubtator.txt.gz"
+MODULE = pystow.module("gilda", "medmentions")
+CORPUS_PATH = MODULE.join(name="corpus.json")
 
 
 def get_corpus():
+    """Get the MedMentions corpus.
+
+    :return: A list of dictionaries of the following form:
+
+    .. code-block::
+
+        [
+          {
+            "id": 25763772,
+            "title_text": "DCTN4 as a modifier of chronic Pseudomonas aeruginosa infection in cystic fibrosis",
+            "abstract_text": "...",
+            "entities": [
+              {
+                "document_id": 25763772,
+                "start_index": 0,
+                "end_index": 5,
+                "text_segment": "DCTN4",
+                "semantic_type_id": "T116,T123",
+                "entity_id": "C4308010"
+              },
+              ...
+            ]
+          },
+          ...
+        ]
+    """
     if not CORPUS_PATH.is_file():
         path = MODULE.ensure(url=URL)
         corpus = from_gz(path)
-        with CORPUS_PATH.open('w') as file:
+        with CORPUS_PATH.open("w") as file:
             json.dump(corpus, file, indent=2, default=lambda o: o.__dict__)
 
     # Right now I'd rather not engage with the strange object structure, so
@@ -28,8 +54,8 @@ def get_corpus():
 @verbose_option
 def main():
     corpus = get_corpus()
-    click.echo(f'There are {len(corpus)} entries')
+    click.echo(f"There are {len(corpus)} entries")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
