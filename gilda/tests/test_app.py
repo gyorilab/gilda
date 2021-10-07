eb444defa91aad1b2cce8a13160199fedcd70876
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-10-07 20:42:44 +0200

    Add a few more tests
    Need a better example that shows off multi-organism grounding, though

--- gilda/tests/test_app.py
+++ gilda/tests/test_app.py
@@ -11,7 +11,21 @@ class TestApp(unittest.TestCase):
     def test_post_grounding(self):
         """Test the POST response with text."""
         with app.test_client() as client:
-            res = client.post(f"/ground", json={"text": "AKT"})
-            self.assertIsNotNone(res)
-            self.assertIsNotNone(res.json)
+            res = client.post("/ground")
+            self.assertIn("message", res.json)
+
+            res = client.post("/ground", json={"text": "AKT1"})
             self.assertIsInstance(res.json, list)
+            self.assert_found(res.json, "HGNC", "391")
+
+            res = client.post("/ground", json={"text": "AKT1", "organisms": ["9606", "10090"]})
+            self.assertIsInstance(res.json, list)
+            self.assert_found(res.json, "HGNC", "391")
+            self.assert_found(res.json, "MGI", "87986")
+
+    def assert_found(self, matches, prefix: str, identifier: str) -> None:
+        match_curies = {
+            (match['term']['db'], match['term']['id'])
+            for match in matches
+        }
+        self.assertIn((prefix, identifier), match_curies)
