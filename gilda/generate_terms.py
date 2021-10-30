e9c041071266d6dea0cc6b605d4f3f35cd48e53e
Ben Gyori <ben.gyori@gmail.com>
2021-10-30 18:44:28 -0400

    Remove unused imports

--- gilda/generate_terms.py
+++ gilda/generate_terms.py
@@ -13,7 +13,7 @@ import itertools
 import indra
 from indra.util import write_unicode_csv
 from indra.databases import hgnc_client, uniprot_client, chebi_client, \
-    go_client, mesh_client, doid_client, efo_client, hp_client
+    go_client, mesh_client, doid_client
 from indra.statements.resources import amino_acids
 from .term import Term
 from .process import normalize
