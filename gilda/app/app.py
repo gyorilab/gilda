2cebb39b04a527c32473810af4ff746c97914f00
galileosteinberg <gsteinberg@brandeis.edu>
2024-07-26 12:30:23 -0400

    Fixed formatting issues

--- gilda/app/app.py
+++ gilda/app/app.py
@@ -51,56 +51,54 @@ term_model = api.model(
         description='The normalized text corresponding to the text entry, '
                     'used for lookups.',
         example='egf receptor'),
-     'text' : fields.String(
-         description='The text entry that was matched.',
-         example='EGF receptor'
+     'text': fields.String(
+        description='The text entry that was matched.',
+        example='EGF receptor'
      ),
-     'db' : fields.String(
-         description='The database / namespace corresponding to the '
-                     'grounded term.',
-         example='HGNC'
+     'db': fields.String(
+        description='The database / namespace corresponding to the '
+                    'grounded term.',
+        example='HGNC'
      ),
      'id': fields.String(
-         description='The identifier of the grounded term within the '
-                     'database / namespace.',
-         example='3236'
+        description='The identifier of the grounded term within the '
+                    'database / namespace.',
+        example='3236'
      ),
      'entry_name': fields.String(
-         description='The standardized name corresponding to the grounded '
-                     'term.',
-         example='EGFR'
+        description='The standardized name corresponding to the grounded '
+                    'term.',
+        example='EGFR'
      ),
      'status': fields.String(
-         description='The relationship of the text entry to the grounded '
-                     'term, e.g., synonym.',
-         example='curated'
+        description='The relationship of the text entry to the grounded '
+                    'term, e.g., synonym.',
+        example='curated'
      ),
      'source': fields.String(
-         description='The source from which the term was obtained.',
-         example='famplex'
+        description='The source from which the term was obtained.',
+        example='famplex'
      ),
      'organism': fields.String(
-         description='If the term is a gene/protein, this field provides '
-                     'the taxonomy identifier of the species to which '
-                     'it belongs.',
-         example='9606'
+        description='If the term is a gene/protein, this field provides '
+                    'the taxonomy identifier of the species to which '
+                    'it belongs.',
+        example='9606'
      ),
      'source_db': fields.String(
-         description='In some cases the term\'s db/id was mapped from another '
-                     'db/id pair given in the original source. If this is the '
-                     'case, this field provides the original source db.'),
+        description='In some cases the term\'s db/id was mapped from another '
+                    'db/id pair given in the original source. If this is the '
+                    'case, this field provides the original source db.'),
      'source_id': fields.String(
-         description='In some cases the term\'s db/id was mapped from another '
-                     'db/id pair given in the original source. If this is the '
-                     'case, this field provides the original source ID.')
-
+        description='In some cases the term\'s db/id was mapped from another '
+                    'db/id pair given in the original source. If this is the '
+                    'case, this field provides the original source ID.')
     }
 )
 
 scored_match_model = api.model(
     "ScoredMatch",
-    {'term': fields.Nested(term_model,
-                           description='The term that was matched'),
+    {'term': fields.Nested(term_model, description='The term that was matched'),
      'url': fields.String(
          description='Identifiers.org URL for the matched term.',
          example='https://identifiers.org/hgnc:3236'
@@ -110,15 +108,15 @@ scored_match_model = api.model(
          example=0.9845
      ),
      'match': fields.Nested(api.model('Match', {}),
-                            description='Additional metadata about the nature of the match.'
-                            ),
-     'subsumed_terms': fields.List(fields.Nested(term_model),
-                                   description='In some cases multiple terms with the same db/id '
-                                               'matched the input string, potentially with different '
-                                               'scores, and only the first one is exposed in the '
-                                               'scored match\'s term attribute (see above). This field '
-                                               'provides additional terms with the same db/id that '
-                                               'matched the input for additional traceability.')
+         description='Additional metadata about the nature of the match.'
+     ),
+    'subsumed_terms': fields.List(fields.Nested(term_model),
+         description='In some cases multiple terms with the same db/id '
+                     'matched the input string, potentially with different '
+                     'scores, and only the first one is exposed in the '
+                     'scored match\'s term attribute (see above). This field '
+                     'provides additional terms with the same db/id that '
+                     'matched the input for additional traceability.')
      }
 )
 
@@ -129,26 +127,26 @@ get_names_input_model = api.model(
                     "e.g. HGNC.",
         required=True,
         example='HGNC'),
-        'id': fields.String(
-            description="Identifier within the given database",
-            required=True,
-            example='3236'
-        ),
-        'status': fields.String(
-            description="If provided, only entity texts of the given status are "
-                        "returned (e.g., curated, name, synonym, former_name).",
-            required=False,
-            enum=['curated', 'name', 'synonym', 'former_name'],
-            example='synonym'
-        ),
-        'source': fields.String(
-            description="If provided, only entity texts collected from the given "
-                        "source are returned.This is useful if terms grounded to "
-                        "IDs in a given database are collected from multiple "
-                        "different sources.",
-            required=False,
-            example='uniprot'
-        )
+    'id': fields.String(
+        description="Identifier within the given database",
+        required=True,
+        example='3236'
+    ),
+    'status': fields.String(
+        description="If provided, only entity texts of the given status are "
+                    "returned (e.g., curated, name, synonym, former_name).",
+        required=False,
+        enum=['curated', 'name', 'synonym', 'former_name'],
+        example='synonym'
+    ),
+    'source': fields.String(
+        description="If provided, only entity texts collected from the given "
+                    "source are returned.This is useful if terms grounded to "
+                    "IDs in a given database are collected from multiple "
+                    "different sources.",
+        required=False,
+        example='uniprot'
+    )
     }
 )
 
@@ -162,8 +160,8 @@ ner_result_model = api.model('NERResult', {
 ner_input_model = api.model('NERInput', {
     'text': fields.String(required=True, description='Text on which to perform'
                                                      ' NER',
-                          example='The EGF receptor binds EGF which is an interaction'
-                                  'important in cancer.'),
+                          example='The EGF receptor binds EGF which is an '
+                                  'interaction important in cancer.'),
     'organisms': fields.List(fields.String, example=['9606'],
                              description='An optional list of taxonomy '
                                          'species IDs defining a priority list'
